d904e47eb6ef9d78781e082397861637ac4d35dd
AmandaLyyy <amandaa34m@gmail.com>
2026-06-17 12:05:38 -0300

    adiciona arquivos para deploy no Railway

--- CreditoProjeto/CreditoProjetoI/app.py
+++ CreditoProjeto/CreditoProjetoI/app.py
@@ -920,5 +920,5 @@ def dashboard():
         total_fornecedores=total_fornecedores)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
     
